2034e83288437cd093226abf84b808338bcf4be4
ColdingPoTaTo <william.9150@gmail.com>
2022-01-27 23:21:40 +0800

    first commit

---
+++ CampWebsite/CampWebsite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CampWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CampWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbCampEntities db = new dbCampEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
