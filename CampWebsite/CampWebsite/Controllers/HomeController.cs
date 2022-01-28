a8820887b3d8675a72075658549f96fe92864860
William <william.9150@gmail.com>
2022-01-28 15:20:45 +0800

    add search function in HomeController, from long

--- CampWebsite/CampWebsite/Controllers/HomeController.cs
+++ CampWebsite/CampWebsite/Controllers/HomeController.cs
@@ -17,5 +17,19 @@ namespace CampWebsite.Controllers
         {
             return View();
         }
+
+        //俊長的營區搜尋
+        public ActionResult search()
+        {
+            IEnumerable<tCampsite> products = null;
+            string keyword = Request.Form["txtKeyword"];
+            if (string.IsNullOrEmpty(keyword))
+                products = from p in (new dbCampEntities()).tCampsite select p;
+            else
+                products = from p in (new dbCampEntities()).tCampsite where p.fCampsiteName.Contains(keyword) select p;
+            //var todos = db.營區資料.OrderByDescending(m =>m.營區Id).ToList();
+
+            return View(products);
+        }
     }
 }
