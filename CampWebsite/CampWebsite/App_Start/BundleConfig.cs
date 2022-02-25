2f193f6b7df32a5446991828b650f18d56b5180f
William <william.9150@gmail.com>
2022-02-25 18:33:13 +0800

    add Bootstrap and jQuery package

---
+++ CampWebsite/CampWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Optimization;
+using System.Linq;
+using System.Web;
+
+namespace CampWebsite
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundleScripts").Include(
+                        "~/Scripts/jQuery/jquery-3.6.0.min.js"));
+            bundles.Add(new ScriptBundle("~/bundleScripts").Include(
+                        "~/Scripts/Bootstrap/bootstrap.bundle.min.js"));
+            
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/themes/base/jquery-ui.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundleCss").Include(
+                      "~/Content/Bootstrap/bootstrap.min.css"));
+
+            BundleTable.EnableOptimizations = true;
+
+            //var jqueryCdnPath = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js";
+            //bundles.Add(new ScriptBundle("~/cdn/jquery", jqueryCdnPath).Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+        }
+    }
+}
--- CampWebsite/CampWebsite/Global.asax.cs
+++ CampWebsite/CampWebsite/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 
@@ -14,6 +16,7 @@ namespace CampWebsite
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
         public MvcApplication()
         {
